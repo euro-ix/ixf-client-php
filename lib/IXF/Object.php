fbb03da731c7d12e49d301552afbd002abfc7024
Barry O'Donovan <barry@opensolutions.ie>
2014-05-07 14:37:24 +0100

    Debugging... remove more cruft

--- lib/IXF/Object.php
+++ lib/IXF/Object.php
@@ -4,48 +4,24 @@ namespace IXF;
 
 class Object implements \ArrayAccess
 {
-    /**
-     * @var array Attributes that should not be sent to the API because they're
-     *    not updatable (e.g. API key, ID).
-     */
-    public static $permanentAttributes;
-
     public static function init()
-    {
-        self::$permanentAttributes = new Util\Set(array('id'));
-    }
+    {}
 
     protected $_values;
-    protected $_unsavedValues;
-    protected $_transientValues;
     protected $_retrieveOptions;
 
     public function __construct($id=null)
     {
         $this->_values = array();
-        $this->_unsavedValues = new Util\Set();
-        $this->_transientValues = new Util\Set();
 
-        $this->_retrieveOptions = array();
-        if (is_array($id)) {
-            foreach ($id as $key => $value) {
-                if ($key != 'id')
-                $this->_retrieveOptions[$key] = $value;
-            }
-            $id = $id['id'];
-        }
-
-        if ($id)
-        $this->id = $id;
+        if( $id )
+            $this->id = $id;
     }
 
     // Standard accessor magic methods
     public function __set($k, $v)
     {
         $this->_values[$k] = $v;
-
-        if (!self::$permanentAttributes->includes($k))
-            $this->_unsavedValues->add($k);
     }
 
     public function __isset($k)
@@ -56,8 +32,6 @@ class Object implements \ArrayAccess
     public function __unset($k)
     {
         unset($this->_values[$k]);
-        $this->_transientValues->add($k);
-        $this->_unsavedValues->discard($k);
     }
 
     public function __get($k)
@@ -66,26 +40,8 @@ class Object implements \ArrayAccess
         {
             return $this->_values[$k];
         }
-        elseif( $this->_transientValues->includes($k) )
-        {
-            $class = get_class($this);
-            $attrs = join(', ', array_keys($this->_values));
-            // IXF-FIXME
-            $message = "IXF Notice: Undefined property of $class instance: $k. "
-            . "HINT: The $k attribute was set in the past, however. "
-            . "It was then wiped when refreshing the object "
-            . "with the result returned by IXF's API, "
-            . "probably as a result of a save(). The attributes currently "
-            . "available on this object are: $attrs";
-            error_log($message);
-
+        else
             return null;
-        } else {
-            $class = get_class($this);
-            error_log("IXF Notice: Undefined property of $class instance: $k");
-
-            return null;
-        }
     }
 
     // ArrayAccess methods
@@ -156,22 +112,10 @@ class Object implements \ArrayAccess
         $removed = array_diff( array_keys( $this->_values ), array_keys( $values ) );
 
         foreach( $removed as $k )
-        {
-            if( self::$permanentAttributes->includes( $k ) )
-                continue;
             unset( $this->$k );
-        }
 
         foreach( $values as $k => $v )
-        {
-            if( self::$permanentAttributes->includes( $k ) )
-                continue;
-
             $this->_values[$k] = Util::convertToIxfObject( $v );
-
-            $this->_transientValues->discard($k);
-            $this->_unsavedValues->discard($k);
-        }
     }
 
     /**
@@ -181,14 +125,13 @@ class Object implements \ArrayAccess
     public function serializeParameters()
     {
         $params = array();
-        if ($this->_unsavedValues) {
-            foreach ($this->_unsavedValues->toArray() as $k) {
-                $v = $this->$k;
-                if ($v === NULL) {
-                    $v = '';
-                }
-                $params[$k] = $v;
-            }
+        foreach( $this->_values->toArray() as $k )
+        {
+            $v = $this->$k;
+            if( $v === NULL )
+                $v = '';
+
+            $params[$k] = $v;
         }
 
         return $params;
